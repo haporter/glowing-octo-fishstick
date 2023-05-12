4b1218265d9289d6977f8a9f1a2921a8e1f33cab
Andrew Porter <haporter@me.com>
2023-05-12 12:31:32 -0600

    Network service fetch resource method implementation

--- IFTTT-challenge/IFTTT-challenge/NetworkService.swift
+++ IFTTT-challenge/IFTTT-challenge/NetworkService.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import Combine
+
+enum NetworkError: Error {
+    case invalidResourcePath
+    case fileLoadError
+}
+
+protocol NetworkServiceType {
+    func fetch<T: Decodable>(_ resource: String) ->  AnyPublisher<T, Error>
+}
+
+struct NetworkService: NetworkServiceType {
+    
+    func fetch<T>(_ resource: String) -> AnyPublisher<T, Error> where T : Decodable {
+        Future { promise in
+            DispatchQueue.global().async {
+                guard let url = Bundle.main.url(forResource: resource, withExtension: "json") else {
+                    return promise(.failure(NetworkError.invalidResourcePath))
+                }
+                
+                guard let data = try? Data(contentsOf: url) else {
+                    return promise(.failure(NetworkError.fileLoadError))
+                }
+                
+                let decoder = JSONDecoder()
+                do {
+                    let loaded = try decoder.decode(T.self, from: data)
+                    promise(.success(loaded))
+                } catch {
+                    promise(.failure(error))
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+}
