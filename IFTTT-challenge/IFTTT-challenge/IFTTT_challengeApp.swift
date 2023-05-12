4ad1e1b9c1ff4042b723f793c475f0245b698624
Andrew Porter <haporter@me.com>
2023-05-11 22:31:10 -0600

    Xcode project added

---
+++ IFTTT-challenge/IFTTT-challenge/IFTTT_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IFTTT_challengeApp.swift
+//  IFTTT-challenge
+//
+//  Created by Andrew Porter on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct IFTTT_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
