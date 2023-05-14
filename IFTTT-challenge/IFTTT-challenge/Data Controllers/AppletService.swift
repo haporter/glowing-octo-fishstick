d1e1893d9a573b2337f9999a9fe87266c42c8235
Andrew Porter <haporter@me.com>
2023-05-14 11:24:28 -0600

    AppletService implementation

--- IFTTT-challenge/IFTTT-challenge/Data Controllers/AppletService.swift
+++ IFTTT-challenge/IFTTT-challenge/Data Controllers/AppletService.swift
@@ -8,6 +8,53 @@
 import Foundation
 import Combine
 
+protocol JSONDecoderType {
+    func decode<T>(_ type: T.Type, from data: Data) throws -> T where T : Decodable
+}
+
 protocol AppletServiceType {
-//    func fetchApplets() -> AnyPublisher<[]
+    func fetchApplets() -> AnyPublisher<[Applet], Error>
+}
+
+struct AppletService: AppletServiceType {
+    
+    let network: NetworkServiceType
+    let decoder: JSONDecoderType
+    
+    init(network: NetworkServiceType = NetworkService(), decoder: JSONDecoderType = JSONDecoder.appletDecoder) {
+        self.network = network
+        self.decoder = decoder
+    }
+    
+    private var liveApplets: some Publisher<DTOApplets, Error> {
+        network
+            .fetch("applets",
+                   decoder: decoder,
+                   bundle: .main)
+            .share()
+    }
+    
+    func fetchApplets() -> AnyPublisher<[Applet], Error> {
+        liveApplets
+            .map(\.liveApplets)
+            .eraseToAnyPublisher()
+    }
+}
+
+private struct DTOApplets: Decodable {
+    let liveApplets: [Applet]
+}
+
+private extension JSONDecoder {
+    static var appletDecoder: JSONDecoder {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        decoder.dateDecodingStrategy = .formatted(dateFormatter)
+        
+        return decoder
+    }
 }
