a3365904b34f54127b1a741803c200230b6608d6
Andrew Porter <haporter@me.com>
2023-05-16 17:20:13 -0600

    View code clean up

--- IFTTT-challenge/IFTTT-challenge/Applet List/AppletListView.swift
+++ IFTTT-challenge/IFTTT-challenge/Applet List/AppletListView.swift
@@ -16,41 +16,47 @@ struct AppletListView: View {
         ScrollView {
             LazyVStack(spacing: 16) {
                 ForEach(viewModel.appletPreview) { applet in
-                    VStack(alignment: .leading) {
-                        HStack {
-                            ForEach(0..<viewModel.channelIcons(for: applet).count, id: \.self) { index in
-                                KFImage(viewModel.channelIcons(for: applet)[index])
-                                    .placeholder({
-                                        Image(systemName: "photo.fill")
-                                    })
-                                    .resizable()
-                                    .frame(width: 24, height: 24)
-                                    .cornerRadius(4)
-                            }
-                            Spacer()
-                            Text(applet.status.prettyName.uppercased())
-                                .font(.system(.callout, weight: .bold))
-                                .foregroundColor(.white)
-                                .padding(.horizontal, 8)
-                                .padding(.vertical, 2)
-                                .background(applet.status.color)
-                                .cornerRadius(4)
-                        }
-                        .padding(.bottom, 24)
-                        
-                        Text(applet.name)
-                            .font(.headline)
-                            .foregroundColor(.white)
-                        Text(applet.author)
-                            .foregroundColor(.white)
-                    }
-                    .padding()
-                    .background(Color(hex: applet.brandColor))
-                    .cornerRadius(8)
+                    view(for: applet)
                 }
             }
             .padding(EdgeInsets(top: 0, leading: 16, bottom: 32, trailing: 16))
         }
+    }
+    
+    func view(for applet: AppletPreview) -> some View {
+        VStack(alignment: .leading) {
+            HStack {
+                ForEach(0..<viewModel.channelIcons(for: applet).count, id: \.self) { index in
+                    KFImage(viewModel.channelIcons(for: applet)[index])
+                        .placeholder({
+                            Image(systemName: "photo.fill")
+                        })
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .cornerRadius(4)
+                }
+                Spacer()
+                Text(applet.status.prettyName.uppercased())
+                    .font(.system(.callout, weight: .bold))
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 2)
+                    .background(applet.status.color)
+                    .cornerRadius(4)
+            }
+            .padding(.bottom, 24)
+            
+            Text(applet.name)
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding(.bottom, 8)
+            Text(applet.author)
+                .font(.callout)
+                .foregroundColor(.white)
+        }
+        .padding()
+        .background(Color(hex: applet.brandColor))
+        .cornerRadius(8)
     }
 }
 
