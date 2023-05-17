26a699b73fafbce99cb3ea57219a0e7affbbf11e
Andrew Porter <haporter@me.com>
2023-05-17 10:18:22 -0600

    Unit test changes

--- IFTTT-challenge/IFTTT-challengeTests/AppletServiceTests.swift
+++ IFTTT-challenge/IFTTT-challengeTests/AppletServiceTests.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+import XCTest
+import Combine
+@testable import IFTTT_challenge
+
+final class AppletServiceTests: XCTestCase {
+    
+    private var sut: AppletService!
+    private var bag = Set<AnyCancellable>()
+    
+    override func setUp() {
+        super.setUp()
+        
+        sut = AppletService(network: MockNetwork())
+    }
+    
+    override func tearDown() {
+        sut = nil
+        bag = []
+        
+        super.tearDown()
+    }
+    
+    func testSomething() {
+        
+    }
+}
+
+struct MockNetwork: NetworkServiceType {
+    var appletPublisher = Just(DTOApplets(liveApplets: [])).setFailureType(to: Error.self).eraseToAnyPublisher()
+    
+    func fetch<T>(_ resource: String, decoder: IFTTT_challenge.JSONDecoderType, bundle: Bundle) -> AnyPublisher<T, Error> where T : Decodable {
+        appletPublisher
+            .map { $0 as! T }
+            .eraseToAnyPublisher()
+    }
+}
+
+extension JSONDecoder {
+    static var appletDecoder: JSONDecoder {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        decoder.dateDecodingStrategy = .formatted(dateFormatter)
+        
+        return decoder
+    }
+}
